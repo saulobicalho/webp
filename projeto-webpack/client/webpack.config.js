00655296253754f2194d1593a797fd88a70eb40e
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-09-07 20:03:10 -0300

    fix: __dirname

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './app-src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(_dirname, 'dist')
+    path: path.resolve(__dirname, 'dist')
 
   }
 }
